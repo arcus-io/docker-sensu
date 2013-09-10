9c25c12703b63583e64c46b1a79cb443b228263b
Evan Hazlett <ejhazlett@gmail.com>
2013-09-10 17:11:54 -0400

    fixed bug with reading configs ; updated to pull external if specified

--- Dockerfile
+++ Dockerfile
@@ -6,22 +6,18 @@ RUN apt-get install -y wget
 RUN wget -q http://repos.sensuapp.org/apt/pubkey.gpg -O- | apt-key add -
 RUN echo "deb http://repos.sensuapp.org/apt sensu main" > /etc/apt/sources.list.d/sensu.list
 RUN apt-get update
-RUN DEBIAN_FRONTEND=noninteractive apt-get install -y sensu rabbitmq-server redis-server supervisor openssh-server ruby rubygems
-RUN wget -O /etc/sensu/plugins/check-procs.rb https://raw.github.com/sensu/sensu-community-plugins/master/plugins/processes/check-procs.rb
-RUN chmod 755 /etc/sensu/plugins/check-procs.rb
+RUN RUNLEVEL=1 DEBIAN_FRONTEND=noninteractive apt-get install -y sensu ca-certificates rabbitmq-server redis-server supervisor ruby rubygems git-core
+RUN git clone https://github.com/sensu/sensu-community-plugins.git /opt/sensu_plugins
+
 RUN gem install sensu-plugin --no-rdoc --no-ri
 
-RUN mkdir /root/.ssh
-RUN mkdir /var/run/sshd
-# NOTE: change this key to your own
-ADD sshkey.pub /root/.ssh/authorized_keys
-ADD config.json /etc/sensu/config.json
-ADD client.json /etc/sensu/conf.d/client.json
+RUN rm -rf /etc/sensu/plugins
+RUN ln -sf /opt/sensu_plugins/plugins/ /etc/sensu/plugins
+RUN chmod +x /etc/sensu/plugins/*
+
 ADD supervisor.conf /opt/supervisor.conf
-RUN chown root:root /root/.ssh/authorized_keys
 ADD run.sh /usr/local/bin/run
 
-EXPOSE 22
 EXPOSE 4567
 EXPOSE 5672
 EXPOSE 6379
--- run.sh
+++ run.sh
@@ -1,3 +1,120 @@
 #!/bin/sh
+DASHBOARD_USER=${DASHBOARD_USER:-admin}
+DASHBOARD_PASS=${DASHBOARD_PASS:-sensu}
+SENSU_CONFIG_URL=${SENSU_CONFIG_URL:-}
+SENSU_CLIENT_CONFIG_URL=${SENSU_CLIENT_CONFIG_URL:-}
+SENSU_CHECKS_CONFIG_URL=${SENSU_CHECKS_CONFIG_URL:-}
+
+if [ ! -z "$SENSU_CONFIG_URL" ] ; then
+    wget --no-check-certificate -O /etc/sensu/config.json $SENSU_CONFIG_URL
+else
+    cat << EOF > /etc/sensu/config.json
+{
+  "rabbitmq": {
+    "port": 5672,
+    "host": "localhost",
+    "user": "guest",
+    "password": "guest",
+    "vhost": "/"
+  },
+  "redis": {
+    "host": "localhost",
+    "port": 6379
+  },
+  "api": {
+    "host": "localhost",
+    "port": 4567
+  },
+  "dashboard": {
+    "host": "localhost",
+    "port": 8080,
+    "user": "$DASHBOARD_USER",
+    "password": "$DASHBOARD_PASS"
+  },
+  "handlers": {
+    "default": {
+      "type": "pipe",
+      "command": "true"
+    }
+  }
+}
+
+EOF
+fi
+
+if [ ! -z "$SENSU_CLIENT_CONFIG_URL" ] ; then
+    wget --no-check-certificate -O /etc/sensu/conf.d/client.json $SENSU_CLIENT_CONFIG_URL
+else
+    cat << EOF > /etc/sensu/conf.d/client.json
+{
+    "client": {
+      "name": "sensu-server",
+      "address": "127.0.0.1",
+      "subscriptions": [ "default", "sensu" ]
+    }
+}
+EOF
+fi
+
+if [ ! -z "$SENSU_CHECKS_CONFIG_URL" ] ; then
+    wget --no-check-certificate -O /etc/sensu/conf.d/checks.json $SENSU_CHECKS_CONFIG_URL
+else
+    cat << EOF > /etc/sensu/conf.d/checks.json
+{
+  "checks": {
+    "sensu-rabbitmq-beam": {
+      "handlers": [
+        "default"
+      ],
+      "command": "/etc/sensu/plugins/check-procs.rb -p beam -C 1 -w 4 -c 5",
+      "interval": 60,
+      "occurrences": 2,
+      "refresh": 300,
+      "subscribers": [ "sensu" ]
+    },
+    "sensu-rabbitmq-epmd": {
+      "handlers": [
+        "default"
+      ],
+      "command": "/etc/sensu/plugins/check-procs.rb -p epmd -C 1 -w 1 -c 1",
+      "interval": 60,
+      "occurrences": 2,
+      "refresh": 300,
+      "subscribers": [ "sensu" ]
+    },
+    "sensu-redis": {
+      "handlers": [
+        "default"
+      ],
+      "command": "/etc/sensu/plugins/check-procs.rb -p redis-server -C 1 -w 4 -c 5",
+      "interval": 60,
+      "occurrences": 2,
+      "refresh": 300,
+      "subscribers": [ "sensu" ]
+    },
+    "sensu-api": {
+      "handlers": [
+        "default"
+      ],
+      "command": "/etc/sensu/plugins/check-procs.rb -p sensu-api -C 1 -w 4 -c 5",
+      "interval": 60,
+      "occurrences": 2,
+      "refresh": 300,
+      "subscribers": [ "sensu" ]
+    },
+    "sensu-dashboard": {
+      "handlers": [
+        "default"
+      ],
+      "command": "/etc/sensu/plugins/check-procs.rb -p sensu-dashboard -C 1 -w 1 -c 1",
+      "interval": 60,
+      "occurrences": 2,
+      "refresh": 300,
+      "subscribers": [ "sensu" ]
+    }
+  }
+}
+EOF
+fi
 
 supervisord -c /opt/supervisor.conf -n
