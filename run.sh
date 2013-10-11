7e7efeba40510ba0c82b9432859a4b97efd7bfcd
Evan Hazlett <ejhazlett@gmail.com>
2013-10-11 16:36:23 -0400

    updated run.sh

--- run.sh
+++ run.sh
@@ -7,40 +7,40 @@ SENSU_CONFIG_URL=${SENSU_CONFIG_URL:-}
 SENSU_CLIENT_CONFIG_URL=${SENSU_CLIENT_CONFIG_URL:-}
 SENSU_CHECKS_CONFIG_URL=${SENSU_CHECKS_CONFIG_URL:-}
 
-if [ ! -z "$SKIP_CONFIG" ]; then
-    if [ ! -z "$SENSU_CONFIG_URL" ] ; then
-        wget --no-check-certificate -O /etc/sensu/config.json $SENSU_CONFIG_URL
-    else
+if [ ! -z "$SENSU_CONFIG_URL" ] ; then
+    wget --no-check-certificate -O /etc/sensu/config.json $SENSU_CONFIG_URL
+else
+    if [ ! -e "/etc/sensu/config.json" ] ; then
         cat << EOF > /etc/sensu/config.json
-    {
-      "rabbitmq": {
-        "port": 5672,
-        "host": "$SENSU_HOST",
-        "user": "guest",
-        "password": "guest",
-        "vhost": "/"
-      },
-      "redis": {
-        "host": "$SENSU_HOST",
-        "port": 6379
-      },
-      "api": {
-        "host": "$SENSU_HOST",
-        "port": 4567
-      },
-      "dashboard": {
-        "host": "$SENSU_HOST",
-        "port": 8080,
-        "user": "$DASHBOARD_USER",
-        "password": "$DASHBOARD_PASS"
-      },
-      "handlers": {
-        "default": {
-          "type": "pipe",
-          "command": "true"
-        }
-      }
+{
+  "rabbitmq": {
+    "port": 5672,
+    "host": "$SENSU_HOST",
+    "user": "guest",
+    "password": "guest",
+    "vhost": "/"
+  },
+  "redis": {
+    "host": "$SENSU_HOST",
+    "port": 6379
+  },
+  "api": {
+    "host": "$SENSU_HOST",
+    "port": 4567
+  },
+  "dashboard": {
+    "host": "$SENSU_HOST",
+    "port": 8080,
+    "user": "$DASHBOARD_USER",
+    "password": "$DASHBOARD_PASS"
+  },
+  "handlers": {
+    "default": {
+      "type": "pipe",
+      "command": "true"
     }
+  }
+}
 
 EOF
     fi
@@ -49,6 +49,7 @@ fi
 if [ ! -z "$SENSU_CLIENT_CONFIG_URL" ] ; then
     wget --no-check-certificate -O /etc/sensu/conf.d/client.json $SENSU_CLIENT_CONFIG_URL
 else
+    if [ ! -e "/etc/sensu/conf.d/client.json" ] ; then
     cat << EOF > /etc/sensu/conf.d/client.json
 {
     "client": {
@@ -58,6 +59,7 @@ else
     }
 }
 EOF
+    fi
 fi
 
 if [ ! -z "$SENSU_CHECKS_CONFIG_URL" ] ; then
